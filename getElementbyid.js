d372cc8781f946f47daa80d7e405c2c58444e317
Muskantasmiya <142614332+Muskantasmiya@users.noreply.github.com>
2024-02-03 21:36:38 +0530

    js added

---
+++ getElementbyid.js
@@ -0,0 +1,20 @@
+const mainHeading = document.getElementById('main-heading');
+mainHeading.innerHTML = "Fruit World";
+mainHeading.style.color = 'orange';
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = 'Please visit us again';
+
+
+
+
+
+
+
